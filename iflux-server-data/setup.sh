665925e628d338a453f9ee07e695800dc277715e
Laurent Prevost <laurent.prevost@lotaris.com>
2015-06-23 13:48:46 +0200

    Try to fix permission issue

---
+++ iflux-server-data/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data
+
+chown -R ifluxslack:ifluxslack slack
+chown -R citizen:citizen citizen
+chown -R viewbox:viewbox viewbox
+chown -R metrics:metrics metrics
+chown -R metrics:publibike publibike
