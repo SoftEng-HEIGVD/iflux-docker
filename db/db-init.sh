4dc76b86d2418a6f4e1828ac9717a9159ea4a84c
Laurent Prevost <laurent.prevost@lotaris.com>
2015-06-23 10:27:09 +0200

    Fixed issue with database setup

--- db/db-init.sh
+++ db/db-init.sh
@@ -4,10 +4,10 @@ chown -R postgres:postgres /tmp/pg-setup
 PGHOST=/tmp/pg-setup gosu postgres pg_ctl -o "-c listen_addresses='' -c unix_socket_directories='/tmp/pg-setup'" -l "/tmp/pg-setup/setup.log" -w start
 
 echo "Creating the iFLUX user"
-PGHOST=/tmp/pg-setup gosu postgres psql -U postgres -c "CREATE USER $DB_USER PASSWORD '${DB_PASS}'"
+PGHOST=/tmp/pg-setup gosu postgres psql -U postgres -c "CREATE USER $IFLUX_PG_DB_USER PASSWORD '${DB_PASS}'"
 
 echo "Creating the iFLUX database"
-PGHOST=/tmp/pg-setup gosu postgres createdb -U postgres -O $DB_USER $DB_NAME
+PGHOST=/tmp/pg-setup gosu postgres createdb -U postgres -O $IFLUX_PG_DB_USER $IFLUX_PG_DB_NAME
 
 echo "Stopping postgres"
 gosu postgres pg_ctl -m fast -w stop
