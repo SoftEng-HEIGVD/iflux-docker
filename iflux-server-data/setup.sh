055630482101e0870f2858701c99cfa837d52ef1
Laurent Prevost <laurent.prevost@heig-vd.ch>
2015-07-13 23:42:53 +0200

    Added paleo component

--- iflux-server-data/setup.sh
+++ iflux-server-data/setup.sh
@@ -6,4 +6,5 @@ chown -R ifluxslack:ifluxslack slack
 chown -R citizen:citizen citizen
 chown -R viewbox:viewbox viewbox
 chown -R metrics:metrics metrics
-chown -R metrics:publibike publibike
+chown -R publibike:publibike publibike
+chown -R paleo:paleo paleo
--- rp/start.sh
+++ rp/start.sh
@@ -7,6 +7,7 @@ sed -i "s/<gateway>/${GATEWAY_PORT_3000_TCP_ADDR}:${GATEWAY_PORT_3000_TCP_PORT}/
 sed -i "s/<iflux>/${IFLUX_PORT_3000_TCP_ADDR}:${IFLUX_PORT_3000_TCP_PORT}/" /etc/nginx/nginx.conf
 sed -i "s/<kibana>/${KIBANA_PORT_5601_TCP_ADDR}:${KIBANA_PORT_5601_TCP_PORT}/" /etc/nginx/nginx.conf
 sed -i "s/<metrics>/${METRICS_PORT_3000_TCP_ADDR}:${METRICS_PORT_3000_TCP_PORT}/" /etc/nginx/nginx.conf
+sed -i "s/<paleo>/${PALEO_PORT_3000_TCP_ADDR}:${PALEO_PORT_3000_TCP_PORT}/" /etc/nginx/nginx.conf
 sed -i "s/<slack>/${SLACK_PORT_3000_TCP_ADDR}:${SLACK_PORT_3000_TCP_PORT}/" /etc/nginx/nginx.conf
 sed -i "s/<viewbox>/${VIEWBOX_PORT_3000_TCP_ADDR}:${VIEWBOX_PORT_3000_TCP_PORT}/" /etc/nginx/nginx.conf
 
