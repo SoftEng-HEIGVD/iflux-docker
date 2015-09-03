983fc998c8adf7b6e580596a812e9d0cd2cf065e
Laurent Prevost <laurent.prevost@heig-vd.ch>
2015-09-03 11:16:40 +0200

    Fixed issue with PG init

--- db/db-init.sh
+++ db/db-init.sh
@@ -1,13 +1,5 @@
-echo "Starting postgres"
-mkdir -p /tmp/pg-setup
-chown -R postgres:postgres /tmp/pg-setup
-PGHOST=/tmp/pg-setup gosu postgres pg_ctl -o "-c listen_addresses='' -c unix_socket_directories='/tmp/pg-setup'" -l "/tmp/pg-setup/setup.log" -w start
-
 echo "Creating the iFLUX user"
-PGHOST=/tmp/pg-setup gosu postgres psql -U postgres -c "CREATE USER $IFLUX_PG_DB_USER PASSWORD '${DB_PASS}'"
+gosu postgres psql -U postgres -c "CREATE USER $IFLUX_PG_DB_USER PASSWORD '${DB_PASS}'"
 
 echo "Creating the iFLUX database"
-PGHOST=/tmp/pg-setup gosu postgres createdb -U postgres -O $IFLUX_PG_DB_USER $IFLUX_PG_DB_NAME
-
-echo "Stopping postgres"
-gosu postgres pg_ctl -m fast -w stop
+gosu postgres createdb -U postgres -O $IFLUX_PG_DB_USER $IFLUX_PG_DB_NAME
